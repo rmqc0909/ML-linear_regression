377ac85226f6defc8f49aa441e88e322f7ce78bf
xiedan <xiedan@xiaoshouyi.com>
2018-04-11 16:34:00 +0800

    去掉注释代码块

--- neural_networks/adaline_learning/train_model.py
+++ neural_networks/adaline_learning/train_model.py
@@ -14,18 +14,18 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
-# ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
-# ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
-# ax[0].set_xlabel('Epochs')
-# ax[0].set_ylabel('log(Sum-squared-error)')
-# ax[0].set_title('Adaline - Learning rate 0.01')
-#
-# ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
-# ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
-# ax[1].set_xlabel('Epochs')
-# ax[1].set_ylabel('Sum-squared-error')
-# ax[1].set_title('Adaline - Learning rate 0.0001')
-# plt.show()
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
 
 # standardized features
 import neural_networks.perc_learning.train_model as tm
