25a6d451fa1593f35f954391d088238f6c8376a2
xiedan <xiedan@xiaoshouyi.com>
2018-05-05 10:06:24 +0800

    决策树算法--修改鸢尾花路径

--- decision_tree/12.1.Iris_DecisionTree.py
+++ decision_tree/12.1.Iris_DecisionTree.py
@@ -24,10 +24,11 @@ iris_class = 'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'
 
 
 if __name__ == "__main__":
+    os.environ["PATH"] += os.pathsep + 'D:\Program Files (x86)\Graphviz2.38\\bin'
     mpl.rcParams['font.sans-serif'] = [u'SimHei']
     mpl.rcParams['axes.unicode_minus'] = False
 
-    path = os.path.abspath('..') + '/regression/10.iris.data'  # 数据文件路径
+    path = '../regression/10.iris.data'  # 数据文件路径
     data = np.loadtxt(path, dtype=float, delimiter=',', converters={4: iris_type})
     # 将数据的0到3列组成x，第四列组成y
     x, y = np.split(data, (4,), axis=1)
@@ -38,18 +39,18 @@ if __name__ == "__main__":
     # 决策树参数估计
     # min_samples_split = 10：如果该结点包含的样本数目大于10，则(有可能)对其分支
     # min_samples_leaf = 10：叶节点最少的样本个数为10个；若将某结点分支后，得到的每个子结点样本数目都大于10，则完成分支；否则，不进行分支
-    model = DecisionTreeClassifier(criterion='entropy', max_depth=6)
-    model = model.fit(x_train, y_train)
+    model = DecisionTreeClassifier(criterion='entropy', max_depth=6, min_samples_leaf=3)
+    model = model.fit(x, y)
     y_test_hat = model.predict(x_test)      # 测试数据
 
     # 保存~
     # dot -Tpng my.dot -o my.png
     # 1、输出
-    # with open('iris.dot', 'w') as f:
-    #     tree.export_graphviz(model, out_file=f)
-    # # 2、给定文件名
-    # # tree.export_graphviz(model, out_file='iris.dot')
-    # # 3、输出为pdf格式
+    with open('F:\code\ml_algorithm_practice\\decision_tree\\iris.dot', 'w') as f:
+        tree.export_graphviz(model, out_file=f)
+    # 2、给定文件名
+    tree.export_graphviz(model, out_file='iris.dot')
+    # 3、输出为pdf格式
     # dot_data = tree.export_graphviz(model, out_file=None, feature_names=iris_feature_E, class_names=iris_class,
     #                                 filled=True, rounded=True, special_characters=True)
     # graph = pydotplus.graph_from_dot_data(dot_data)
@@ -59,46 +60,47 @@ if __name__ == "__main__":
     # f.close()
 
     # 画图
-    N, M = 5, 5  # 横纵各采样多少个值
-    x1_min, x1_max = x[:, 0].min(), x[:, 0].max()  # 第0列的范围
-    x2_min, x2_max = x[:, 1].min(), x[:, 1].max()  # 第1列的范围
-    t1 = np.linspace(x1_min, x1_max, N)
-    t2 = np.linspace(x2_min, x2_max, M)
-    x1, x2 = np.meshgrid(t1, t2)  # 生成网格采样点
-    x_show = np.stack((x1.flat, x2.flat), axis=1)  # 测试点
-
-    # # 无意义，只是为了凑另外两个维度
-    # # 打开该注释前，确保注释掉x = x[:, :2]
-    # x3 = np.ones(x1.size) * np.average(x[:, 2])
-    # x4 = np.ones(x1.size) * np.average(x[:, 3])
-    # x_test = np.stack((x1.flat, x2.flat, x3, x4), axis=1)  # 测试点
-
-    cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
-    cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
-    y_show_hat = model.predict(x_show)  # 预测值
-    # print y_show_hat
-    y_show_hat = y_show_hat.reshape(x1.shape)  # 使之与输入的形状相同
-    # print y_show_hat
-    plt.figure(facecolor='w')
-    plt.pcolormesh(x1, x2, y_show_hat, cmap=cm_light)  # 预测值的显示
-    plt.scatter(x_test[:, 0], x_test[:, 1], c=y_test.ravel(), edgecolors='k', s=120, cmap=cm_dark, marker='*')  # 测试数据
-    plt.scatter(x[:, 0], x[:, 1], c=y.ravel(), edgecolors='k', s=40, cmap=cm_dark)  # 全部数据
-    plt.xlabel(iris_feature[0], fontsize=15)
-    plt.ylabel(iris_feature[1], fontsize=15)
-    plt.xlim(x1_min, x1_max)
-    plt.ylim(x2_min, x2_max)
-    plt.grid(True)
-    plt.title(u'鸢尾花数据的决策树分类', fontsize=17)
-    plt.show()
-
-    # 训练集上的预测结果
-    y_test = y_test.reshape(-1)
-    print y_test_hat
-    print y_test
-    result = (y_test_hat == y_test)   # True则预测正确，False则预测错误
-    acc = np.mean(result)
-    print("acc is ", acc)
-    print '准确度: %.2f%%' % (100 * acc)
+    # N, M = 5, 5  # 横纵各采样多少个值
+    # x1_min, x1_max = x[:, 0].min(), x[:, 0].max()  # 第0列的范围
+    # x2_min, x2_max = x[:, 1].min(), x[:, 1].max()  # 第1列的范围
+    # t1 = np.linspace(x1_min, x1_max, N)
+    # t2 = np.linspace(x2_min, x2_max, M)
+    # x1, x2 = np.meshgrid(t1, t2)  # 生成网格采样点
+    # x3 = (x1.flat, x2.flat)
+    # x_show = np.stack((x1.flat, x2.flat), axis=1)  # 测试点
+    # # # 无意义，只是为了凑另外两个维度
+    # # # 打开该注释前，确保注释掉x = x[:, :2]
+    # # x3 = np.ones(x1.size) * np.average(x[:, 2])
+    # # x4 = np.ones(x1.size) * np.average(x[:, 3])
+    # # x_test = np.stack((x1.flat, x2.flat, x3, x4), axis=1)  # 测试点
+    #
+    # cm_light = mpl.colors.ListedColormap(['#A0FFA0', '#FFA0A0', '#A0A0FF'])
+    # cm_dark = mpl.colors.ListedColormap(['g', 'r', 'b'])
+    # y_show_hat = model.predict(x_show)  # 预测值
+    # y_show_hat = y_show_hat.reshape(x1.shape)  # 使之与输入的形状相同
+    # print 'y_show_hat = \n', y_show_hat
+    # print 'x1 = \n', x1
+    # print 'x2 = \n', x2
+    # plt.figure(facecolor='w')
+    # plt.pcolormesh(x1, x2, y_show_hat, cmap=cm_light)  # 预测值的显示
+    # # plt.scatter(x_test[:, 0], x_test[:, 1], c=y_test.ravel(), edgecolors='k', s=120, cmap=cm_dark, marker='*')  # 测试数据
+    # plt.scatter(x[:, 0], x[:, 1], c=y.ravel(), edgecolors='k', s=40, cmap=cm_dark)  # 全部数据
+    # plt.xlabel(iris_feature[0], fontsize=15)
+    # plt.ylabel(iris_feature[1], fontsize=15)
+    # plt.xlim(x1_min, x1_max)
+    # plt.ylim(x2_min, x2_max)
+    # plt.grid(True)
+    # plt.title(u'鸢尾花数据的决策树分类', fontsize=17)
+    # plt.show()
+    #
+    # # 训练集上的预测结果
+    # y_test = y_test.reshape(-1)
+    # print y_test_hat
+    # print y_test
+    # result = (y_test_hat == y_test)   # True则预测正确，False则预测错误
+    # acc = np.mean(result)
+    # print("acc is ", acc)
+    # print '准确度: %.2f%%' % (100 * acc)
     #
     # # 过拟合：错误率
     # depth = np.arange(1, 15)
